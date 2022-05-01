c7f39ca8203b2daa6cc513a68d6604f6ab4773b3
Ido Kanner <idok@linesip.com>
2022-05-01 12:43:58 +0300

    trying to figure out stuf and how they work in nvims lua

--- init.lua
+++ init.lua
@@ -1,21 +1,11 @@
-local init_path = debug.getinfo(1, "S").source:sub(2)
-local base_dir = init_path:match("(.*[/\\])"):sub(1, -2)
-
-require 'lua.plugins'
+package.cpath = '~/.config/nvim/lua;' .. package.cpath
 
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
 
-require 'impatient'
+require('packages')
+require('options')
 
-local g = vim.g
+-- require 'impatient'
 
--- Leader/local leader
-g.mapleader = [[\\]]
-g.maplocalleader = [[\\]]
 
--- Skip some remote provider loading
-g.loaded_python_provider = 0
-
-g.python3_host_prog = '/usr/bin/python3'
-g.node_host_prog = '/usr/bin/neovim-node-host'
