8c44c0fc0d2dc3563e10dad05a7d6a09473b4f29
Ido Kanner <idok@linesip.com>
2022-05-15 17:09:01 +0300

    some settings for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
-local vim = require('nvim')
+local lsp_utils = require "lspconfig/util"
 local nvim_lsp = require('lspconfig')
 local nvim_lsp_installer = require("nvim-lsp-installer")
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.formatting = true
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 nvim_lsp_installer.setup({
      -- automatically detect which servers to install (based on which servers
@@ -16,6 +20,7 @@ nvim_lsp_installer.setup({
 })
 
 nvim_lsp.sumneko_lua.setup {
+  capabilities = capabilities,
   settings = {
     runtime = {
       version = 'Lua 5.1',
@@ -46,8 +51,6 @@ nvim_lsp.sumneko_lua.setup {
     },
     format = {
       enable = true,
-      -- Put format options here
-      -- NOTE: the value should be STRING!!
       defaultConfig = {
         indent_style = "space",
         indent_size = "2",
@@ -57,6 +60,7 @@ nvim_lsp.sumneko_lua.setup {
 }
 
 nvim_lsp.ccls.setup {
+  capabilities = capabilities,
   init_options = {
     compilationDatabaseDirectory = "build",
     index = {
@@ -78,6 +82,7 @@ nvim_lsp.ccls.setup {
 }
 
 nvim_lsp.jedi_language_server.setup{
+  capabilities = capabilities,
   markupKindPreferred = 'plaintext',
   jediSettings = {
     autoImportModules = {},
@@ -121,6 +126,9 @@ nvim_lsp.jedi_language_server.setup{
 }
 
 nvim_lsp.lemminx.setup {
+  filetypes = {
+    "xml", "xsd", "xsl", "xslt", "svg", 'wsdl',
+  },
   useCache = true,
   trace = {
     server = "verbose",
@@ -138,9 +146,7 @@ nvim_lsp.lemminx.setup {
     spaceBeforeEmptyCloseTag = true,
     formatComments = true,
   },
-  capabilities = {
-    formatting = true,
-  },
+  capabilities = capabilities,
   completion = {
     autoCloseTags = true,
   },
@@ -151,9 +157,131 @@ nvim_lsp.lemminx.setup {
   },
 }
 
-nvim_lsp.pylsp.setup {
-  configurationSources = { "pycodestyle" },
-
+nvim_lsp.gopls.setup {
+  capabilities = capabilities,
+  cmd = {
+    "gopls",
+    "serve",
+  },
+  filetypes = {
+    "go",
+    "gomod",
+  },
+  initializationOptions = {
+    usePlaceholders = true,
+  },
+  root_dir = lsp_utils.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      staticcheck = true,
+      -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+      analyses = {
+        assign = true,
+        atomic = true,
+        atomicalign = true,
+        bools = true,
+        buildtag = true,
+        cgocall = true,
+        composites = true,
+        copylocks = true,
+        deepequalerrors = true,
+        embed = true,
+        errorsas = true,
+        fieldalignment = true,
+        httpresponse = true,
+        ifaceassert = true,
+        infertypeargs = true,
+        loopclosure = true,
+        lostcancel = true,
+        nilfunc = true,
+        nilness = true,
+        printf = true,
+        shadow = true,
+        shift = true,
+        simplifycompositelit = true,
+        simplifyrange = true,
+        simplifyslice = true,
+        sortslice = true,
+        stdmethods = true,
+        stringintconv = true,
+        structtag = true,
+        testinggoroutine = true,
+        tests = true,
+        unmarshal = true,
+        unreachable = true,
+        unsafeptr = true,
+        unusedparams = true,
+        unusedresult = true,
+        unusedwrite = true,
+        useany = true,
+        fillreturns = true,
+        nonewvars = true,
+        noresultvalues = true,
+        undeclaredname = true,
+        fillstruct = true,
+        stubmethods = true,
+      },
+      codelenses = {
+        generate = true,
+        gc_details = true,
+      },
+      documentation = {
+        hoverKind = 'FullDocumentation',
+        linkTarget = 'pkg.go.dev',
+        linksInHover = true,
+      },
+      Navigation = {
+        importShortcut = 'Both',
+      },
+    },
+  },
 }
 
+nvim_lsp.html.setup {
+  capabilities = capabilities,
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+    provideFormatter = true,
+  },
+}
 
+nvim_lsp.jsonls.setup {
+  capabilities = capabilities,
+  init_options = {
+    enable = true,
+    provideFormatter = true,
+    single_file_support = true,
+  },
+  filetypes = { "json", "jsonc", "json5", "hjson" },
+}
+
+nvim_lsp.pylsp.setup {
+  configurationSources = { "pycodestyle" },
+  capabilities = capabilities,
+}
+
+nvim_lsp.sorbet.setup{
+  capabilities = capabilities,
+  filetypes = {
+      "ruby",
+  },
+  root_dir = lsp_utils.root_pattern("Gemfile", ".git"),
+}
+
+nvim_lsp.sqlls.setup{
+  capabilities = capabilities,
+  filetypes = {
+    "sql", "plsql", "mysql",
+  },
+}
+
+nvim_lsp.sqls.setup{
+  capabilities = capabilities,
+  filetypes = {
+    "sql", "plsql", "mysql",
+  },
+}
