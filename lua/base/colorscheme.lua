b094c6f3eba416a6b2a517278ec5f13210505726
Ido Kanner <idok@linesip.com>
2022-05-19 16:31:11 +0300

    forgot to save this file

--- lua/base/colorscheme.lua
+++ lua/base/colorscheme.lua
@@ -25,26 +25,6 @@ highlight WhitespaceEOL ctermbg=red guibg=red
 match WhitespaceEOL /\s\+$/
 ]])
 
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = true
-  },
-  ensure_installed = {
-    'markdown',
-    'rst',
-  },
-  indent = {
-    enable = true,
-  },
-}
-
 --[[
 local monokai = require('monokai')
 local palette = monokai.classic
