5f1b5bbc053b4749e8dd33780d682940b07019d3
Ido Kanner <idok@linesip.com>
2022-11-22 18:41:13 +0200

    managing better packer and some of its packages

--- lua/base/packages.lua
+++ lua/base/packages.lua
@@ -1,3 +1,21 @@
+
+
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+end
+
 vim.cmd([[packadd packer.nvim]])
 
 vim.cmd([[
@@ -7,7 +25,22 @@ vim.cmd([[
   augroup end
 ]])
 
-return require("packer").startup({
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return packer.startup({
   function()
     -- Base
     use "lewis6991/impatient.nvim"
@@ -24,6 +57,22 @@ return require("packer").startup({
       'nvim-treesitter/nvim-treesitter-context',
       requires = { 'nvim-treesitter/nvim-treesitter' },
     }
+
+    use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup({
+        window = {
+          border = "single"
+        },
+        plugins = {
+          spelling = {
+            enabled = true,
+          },
+        },
+      })
+    end
+  }
 
     use {
       'nvim-lualine/lualine.nvim',
@@ -137,21 +186,17 @@ return require("packer").startup({
       'f3fora/cmp-spell',
       'andersevenrud/cmp-tmux',
       'ray-x/cmp-treesitter',
-      "octaltree/cmp-look",
+      'octaltree/cmp-look',
+      'saadparwaiz1/cmp_luasnip',
     }
 
-    use {
-      'SirVer/ultisnips',
+    -- snippets
+    use { --snippet engine
+      'L3MON4D3/LuaSnip',
       requires = {
-        'honza/vim-snippets',
-      },
-    }
-
-    use {
-      "quangnguyen30192/cmp-nvim-ultisnips",
-      requires = {
-        'SirVer/ultisnips',
-        "hrsh7th/nvim-cmp",
+        'rafamadriz/friendly-snippets', -- a bunch of snippets to use
+        'hrsh7th/nvim-cmp',
+        'saadparwaiz1/cmp_luasnip',
       },
     }
 
