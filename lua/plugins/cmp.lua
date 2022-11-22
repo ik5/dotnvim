9768a4610781eff5e0445a9f0bc94c8e902f2785
Ido Kanner <idok@linesip.com>
2022-11-22 20:15:51 +0200

    rewrite of cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,102 +1,147 @@
-local cmp = require 'cmp'
-local lspkind = require 'lspkind'
-local ultisnips = require "cmp_nvim_ultisnips"
---[[
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
+local fn = vim.fn
+
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
 end
 
-local check_back_space = function()
-    local col = vim.fn.col(".") - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
 end
 
-ultisnips.setup {}
-local cmp_ultisnips_mappings = ultisnips.mappings
---]]
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local check_backspace = function()
+  local col = fn.col "." - 1
+  return col == 0 or fn.getline("."):sub(col, col):match "%s"
+end
+
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup {
   snippet = {
     expand = function(args)
-      vim.fn["UltiSnips#Anon"](args.body)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    -- Accept currently selected item. If none selected, `select` first item.
+    -- Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif check_backspace() then
+        fallback()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[LUA]",
+        path = "[PATH]",
+        omni = "[OMNI]",
+        cmdline = "[CMDLINE]",
+        spell = "[SPELL]",
+        tmux = '[TMUX]',
+        treesitter = '[TS]',
+        luasnip = '[SNIPS]',
+        look = '[LOOK]',
+      })[entry.source.name]
+      return vim_item
     end,
   },
   sources = {
-    { name = 'nvim_lsp', group_index = 1, },
-    { name = 'omni', group_index = 1, },
-    { name = 'treesitter', group_index = 1, },
-    { name = 'nvim-lua', group_index = 1, },
-    { name = 'buffer', group_index = 1, },
-    { name = 'path', group_index = 2, },
- --   { name = 'cmdline', group_index = 2, },
-    { name = 'spell', group_index = 2, },
-    { name = 'ultisnips', group_index = 1, },
-    {
-      name = 'look', group_index = 2,
-      keyword_length = 2,
-      option = {
-        convert_case = true,
-        loud = true -- Convert the candidates to UPPERCASE if all input characters are uppercase.
-      },
-    },
-    {
-      name = 'tmux',
-      group_index = 2,
-      option = {
-        all_panes = true,
-        trigger_characters = { '.' },
-        trigger_characters_ft = {} -- { filetype = { '.' } }
-      },
-    },
+    {name = 'nvim_lsp'},
+    {name = "luasnip"},
+    {name = "nvim_lua"},
+    {name = 'buffer'},
+    {name = "omnini"},
+    {name = "treesitter"},
+    {name = "look"},
+    {name = "tmux"},
+    {name = "path"},
+    {name = "spell"},
+    {name = "emoji"},
   },
-
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-        vim_item.menu = ({
-          buffer = "[BUF]",
-          nvim_lsp = "[LSP]",
-          nvim_lua = "[LUA]",
-          path = "[PATH]",
-          omni = "[OMNI]",
-  --        cmdline = "[CMDLINE]",
-          spell = "[SPELL]",
-          tmux = '[TMUX]',
-          treesitter = '[TS]',
-          ultisnips = '[SNIPS]',
-          look = '[LOOK]',
-        })[entry.source.name]
-        return vim_item
-      end
-    })
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = true,
   },
-
   window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+      documentation = {
+        border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      },
   },
-
-  completion = {
-    keyword_length = 2,
-    completeopt = 'menu,menuone,noinsert',
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
   },
-
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true
-    }),
-  }),
-
 }
+
