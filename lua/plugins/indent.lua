017878837a3fcf57c1b1137e65819f17b06940ae
Ido Kanner <idok@linesip.com>
2022-12-16 19:19:43 +0200

    set color for each identiation line

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,6 +1,13 @@
 
 local indent = require('indent_blankline')
 
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
 indent.setup{
   show_current_context = true,
   show_current_context_start = true,
@@ -9,5 +16,13 @@ indent.setup{
   use_treesitter = true,
   use_treesitter_scope = true,
   space_char_blankline = " ",
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
+  },
 }
 
