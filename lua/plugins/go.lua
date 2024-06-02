9f59b8d9f65f8b8506ee07eed838938aa478b2fc
Ido Kanner <idok@linesip.com>
2024-06-02 14:39:48 +0300

    working on re-configuring golang again

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -8,83 +8,39 @@ go_nvim.setup {
   icons = { breakpoint = "🧘", currentpos = "🏃" }, -- set to false to disable this option
   trouble = true, -- true: use trouble to open quickfix
   iferr_vertical_shift = 4, -- defines where the cursor will end up vertically from the begining of if err statement after GoIfErr command
---[[
-  goimport = 'gopls', -- if set to 'gopls' will use golsp format
-  gofmt = 'gopls', -- if set to gopls will use golsp format
-  fillstruct = "gopls",
-  max_line_len = 120,
-  tag_transform = false,
-  test_dir = '',
-  comment_placeholder = ' <>  ',
-  lsp_cfg = {
-    capabilities = capabilities,
-  },
   lsp_inlay_hints = {
     enable = true,
-
+    -- hint style, set to 'eol' for end-of-line hints, 'inlay' for inline hints
+    -- inlay only available for 0.10.x
+    style = 'inlay',
+    -- Note: following setup only works for style = 'eol', you do not need to set it for 'inlay'
     -- Only show inlay hints for the current line
     only_current_line = false,
-
     -- Event which triggers a refersh of the inlay hints.
     -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
     -- not that this may cause higher CPU usage.
     -- This option is only respected when only_current_line and
     -- autoSetHints both are true.
     only_current_line_autocmd = "CursorHold",
-
     -- whether to show variable name before type hints with the inlay hints or not
     -- default: false
     show_variable_name = true,
-
     -- prefix for parameter hints
-    parameter_hints_prefix = " ",
+    parameter_hints_prefix = "󰊕 ",
     show_parameter_hints = true,
-
     -- prefix for all the other hints (type, chaining)
     other_hints_prefix = "=> ",
-
-    -- whether to align to the lenght of the longest line in the file
+    -- whether to align to the length of the longest line in the file
     max_len_align = false,
-
     -- padding from the left if max_len_align is true
     max_len_align_padding = 1,
-
     -- whether to align to the extreme right or not
     right_align = false,
-
     -- padding from the right if right_align is true
     right_align_padding = 6,
-
     -- The color of the hints
     highlight = "Comment",
   },
-  gopls_remote_auto = true,
-  gocoverage_sign = "󰇉 ",
-  sign_priority = 7,
-  dap_debug = true,
-  dap_debug_gui = true,
-  dap_debug_keymap = true, -- true: use keymap for debugger defined in go/dap.lua
-                           -- false: do not use keymap in go/dap.lua.  you must define your own.
-                           -- windows: use visual studio style of keymap
-  dap_vt = true, -- false, true and 'all frames'
-  textobjects = true,
-  gopls_cmd = nil, --- you can provide gopls path and cmd if it not in PATH, e.g. cmd = {  "/home/ray/.local/nvim/data/lspinstall/go/gopls" }
-  build_tags = "", --- you can provide extra build tags for tests or debugger
-  test_runner = "go", -- one of {`go`, `richgo`, `dlv`, `ginkgo`}
-  luasnip = true, -- set false to disable
-  icons = { breakpoint = "🧘", currentpos = "🏃" }, -- set to false to disable this option
-  trouble = true, -- true: use trouble to open quickfix
-  iferr_vertical_shift = 4, -- defines where the cursor will end up vertically from the begining of if err statement after GoIfErr command
-}
-
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
---]]
 }
 
 --[[ Do not use Faith's vim-go settings, will be removed after configuring new vim plugin
