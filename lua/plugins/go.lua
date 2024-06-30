f2a3bdec9344f63cb4a44fdbb5a34760fee77ab3
Ido Kanner <idok@linesip.com>
2024-06-30 13:10:44 +0300

    fixed warnings

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,7 +1,7 @@
 local go_nvim = require 'go'
 local cfg = vim.g
 go_nvim.setup {
-  goimport = 'gopls', -- if set to 'gopls' will use golsp format
+  goimports = 'gopls', -- if set to 'gopls' will use golsp format
   gofmt = 'gopls', -- if set to gopls will use golsp format
   fillstruct = "gopls",
   luasnip = true, -- set false to disable
